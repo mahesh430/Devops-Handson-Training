667d2070dd1fc57bcbe72ffa9d364474eb2382ba
mahesh430 <umamahesh690@gmail.com>
2024-06-14 10:22:02 +0530

    Create main.tf

---
+++ Lab/ec2-with-sg/main.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "ec2_sg" {
+  name_prefix = var.security_group_name
+  description = "Security group for EC2 instance"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  security_groups = [aws_security_group.ec2_sg.name]
+
+  tags = {
+    Name = var.instance_name
+  }
+}
