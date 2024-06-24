d425eee96f93fd1027f534bf4520c6c1f650736a
mahesh430 <umamahesh690@gmail.com>
2024-06-24 20:16:21 +0530

    Create variables.tf

---
+++ Terraform/Day-32/variables.tf
@@ -0,0 +1,30 @@
+
+variable "ami_ids" {
+  description = "Map of AMI IDs for different regions"
+  type        = map(string)
+  default     = {
+    us-east-1      = "ami-0c55b159cbfafe1f0"
+    us-west-1      = "ami-0abcdef1234567890"
+    eu-west-1      = "ami-0abcdef1234567890"
+    ap-southeast-1 = "ami-0abcdef1234567890"
+  }
+}
+
+variable "security_group_rules" {
+  description = "Map of security group rules"
+  type        = map(map(string))
+  default = {
+    ssh = {
+      from_port = "22"
+      to_port   = "22"
+      protocol  = "tcp"
+      cidr      = "0.0.0.0/0"
+    }
+    http = {
+      from_port = "80"
+      to_port   = "80"
+      protocol  = "tcp"
+      cidr      = "0.0.0.0/0"
+    }
+  }
+}
