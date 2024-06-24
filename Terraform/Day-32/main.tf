871a34c0a0e86a0abf212f6ff92716e5a8b6f4d4
mahesh430 <umamahesh690@gmail.com>
2024-06-24 20:20:58 +0530

    Create main.tf

---
+++ Terraform/Day-32/main.tf
@@ -0,0 +1,49 @@
+# main.tf
+provider "aws" {
+  region = "us-east-1" # Specify the desired region
+}
+
+# Security group creation using dynamic blocks
+resource "aws_security_group" "dynamic_sg" {
+  name        = "dynamic-sg"
+  description = "Dynamic Security Group"
+  vpc_id      = "vpc-123456"  # Replace with your VPC ID
+
+  dynamic "ingress" {
+    for_each = var.security_group_rules
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.protocol
+      cidr_blocks = [ingress.value.cidr]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Single instance creation using the map variable
+resource "aws_instance" "example" {
+  ami           = var.ami_ids[var.region]
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.dynamic_sg.id]
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
+
+output "example_instance_id" {
+  description = "The ID of the example EC2 instance"
+  value       = aws_instance.example.id
+}
+
+output "dynamic_security_group_id" {
+  description = "The ID of the dynamic security group"
+  value       = aws_security_group.dynamic_sg.id
+}
